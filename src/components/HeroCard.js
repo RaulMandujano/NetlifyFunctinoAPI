5a6687f2c3daae699875a5d99a465a6c5b8586d3
Raul <raulmandujanoleon@gmail.com>
2021-12-13 11:43:15 -0700

    updates

--- src/components/HeroCard.js
+++ src/components/HeroCard.js
@@ -1,77 +1,70 @@
-import React,{useEffect} from 'react';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
+import React, { useEffect } from 'react'
+import Card from '@mui/material/Card'
+import CardActions from '@mui/material/CardActions'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
 
-import Typography from '@mui/material/Typography';
-import HeroModal from './HeroModal';
-import HoverRating from './HoverRating';
-import { CardActionArea } from '@mui/material';
+import Typography from '@mui/material/Typography'
+import HeroModal from './HeroModal'
+import HoverRating from './HoverRating'
+import { CardActionArea } from '@mui/material'
 
-import IconButton from '@mui/material/IconButton';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import IconButton from '@mui/material/IconButton'
 
-import { useHistory } from 'react-router-dom';
-
-
+import { useHistory } from 'react-router-dom'
 
 const HeroCard = (props) => {
+  const {
+    id,
+    name,
+    images,
+    family,
+    combat,
+    durability,
+    intelligence,
+    power,
+    speed,
+    strength,
+  } = props
 
-    const { id, name, images, family, combat, durability, intelligence, power, speed, strength } = props;
+  const history = useHistory()
 
-    const history = useHistory()
+  const handleInfoClick = () => {
+    history.push(`/herodetails/${id}`)
+  }
 
-
-    const handleInfoClick = () => {
-      history.push(`/herodetails/${id}`)
-    }
-
-    useEffect(() => {
-
-
-    }, [])
+  useEffect(() => {}, [])
   return (
-    <Card sx={{ maxWidth: 290, float: 'left', margin: "15px", }}>
+    <Card sx={{ maxWidth: 290, float: 'left', margin: '15px' }}>
       <CardActionArea onClick={handleInfoClick}>
-      <CardMedia
-        component="img"
-        height="440"
-        image={images}
-        alt="heroes"
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {name}
-        </Typography>
+        <CardMedia component="img" height="440" image={images} alt="heroes" />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {name}
+          </Typography>
 
-        <Typography sx={{height: 100}} variant="h6" color="text.secondary">
-          {family}
-        </Typography>
-      </CardContent>
+          <Typography sx={{ height: 100 }} variant="h6" color="text.secondary">
+            {family}
+          </Typography>
+        </CardContent>
       </CardActionArea>
 
-
       <CardActions>
-        
-      <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
+        <IconButton aria-label="add to favorites"></IconButton>
 
-        <HoverRating/>
+        <HoverRating />
 
-
-        <HeroModal 
-            combat={combat}
-            durability={durability}
-            intelligence={intelligence}
-            power={power}
-            speed={speed}
-            strength={strength}
+        <HeroModal
+          combat={combat}
+          durability={durability}
+          intelligence={intelligence}
+          power={power}
+          speed={speed}
+          strength={strength}
         />
       </CardActions>
     </Card>
-  );
+  )
 }
 
-export default HeroCard;
+export default HeroCard
